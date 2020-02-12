aba675d490d204963fd8c46e80ac4debb36822ab
Quentin Michel <quentinmichel69110@gmail.com>
2020-02-12 09:47:09 +0100

    basic material ui

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,59 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import styles from './App.css';
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/core/Menu";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import {makeStyles} from "@material-ui/styles";
+import ThemeProvider from "@material-ui/styles/ThemeProvider";
+import createMuiTheme from "@material-ui/core/styles/createMuiTheme";
 
-export const App = () => {
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#2bce3e',
+      // contrastText: '#fff',
+    },
+  },
+});
+
+export const Root = () => {
   return (
-    <div className={styles.App}>
+    <ThemeProvider theme={theme}>
+      <App/>
+    </ThemeProvider>
+  );
+};
 
-    </div>
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
+
+const App = () => {
+  const classes = useStyles();
+  return (
+      <div className={styles.App}>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+              <MenuIcon/>
+            </IconButton>
+            <Typography variant="h6" className={classes.title}>
+              News
+            </Typography>
+            <Button color="inherit">Login</Button>
+          </Toolbar>
+        </AppBar>
+      </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -2,15 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./style/config.css";
 import "./style/style.css";
-import {App} from "./components/App/App";
+import {Root} from "./components/App/App";
 
 const mountNode = document.getElementById("app");
-ReactDOM.render(<App/>, mountNode);
-
-Reveal.initialize({
-  controls: false,
-  overview: false,
-  hash: true,
-  transitionSpeed: 'fast',
-  margin: -0.3, // to adjust when generating a pdf --- .yarn/bin/decktape http://localhost:1234/#/ Courses/A3/SIS/Ghidra/slides/test.pdf --slides 1-3
-});
+ReactDOM.render(<Root/>, mountNode);
