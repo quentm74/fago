5d0cd0d1f1a2a7fbce437afea18312c3f0251db1
Quentin Michel <quentinmichel69110@gmail.com>
2020-02-13 16:04:52 +0100

    add results

--- src/components/App/Results.js
+++ src/components/App/Results.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import Typography from "@material-ui/core/Typography";
 import {makeStyles} from "@material-ui/styles";
 import Grid from '@material-ui/core/Grid';
@@ -8,7 +8,7 @@ import InputBase from '@material-ui/core/InputBase';
 import IconButton from '@material-ui/core/IconButton';
 import SearchIcon from '@material-ui/icons/Search';
 import Button from "@material-ui/core/Button";
-import {Check, MyLocation, Place} from "@material-ui/icons";
+import {Check, Delete, MyLocation, Place, Sync} from "@material-ui/icons";
 import fago from "./fago.png";
 
 const useStyles = makeStyles(theme => ({
@@ -71,10 +71,50 @@ const useStyles = makeStyles(theme => ({
       height: '100%',
     }
   },
+  gridIcons: {
+    display: 'flex',
+  },
+  gridItemtype: {
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  paperIcon: {
+    height: 80,
+    width: 80,
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#2bce3e',
+    marginRight: 10,
+  },
+  icons: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    marginLeft: 10,
+    marginRight: -5,
+    width: '100%',
+  },
+  paperInfos: {
+    padding: 20,
+    width: '100%',
+  },
+  deleteY: {
+    color: 'yellow',
+  },
 }));
+
+const LOCALIZATION = Object.freeze({
+  NONE: 0,
+  RENNES: 1,
+  DEFAULT: 2,
+});
 
 export const Results = () => {
   const classes = useStyles();
+
+  const [localization, setLocalization] = useState(LOCALIZATION.RENNES);
+
   return (
     <div className={classes.root}>
       <div className={classes.found}>
@@ -114,9 +154,66 @@ export const Results = () => {
       <div className={classes.dividerContainer}>
         <Divider orientation="horizontal"/>
       </div>
-      <div className={classes.imgContainer}>
-        <img src={fago} alt="fago"/>
-      </div>
+      {localization === LOCALIZATION.NONE && (
+        <div className={classes.imgContainer}>
+          <img src={fago} alt="fago"/>
+        </div>
+      )}
+      {localization === LOCALIZATION.RENNES && (
+        <Grid container spacing={3} className={classes.grid}>
+          <Grid item xs={2} className={classes.gridItemtype}>
+            <Typography color="primary" variant="h4">Bouteille</Typography>
+          </Grid>
+          <Grid item xs={3} className={classes.gridIcons}>
+            <Divider orientation="vertical"/>
+            <div className={classes.icons}>
+              <Paper className={classes.paperIcon}>
+                <Sync fontSize="large" className={classes.sync}/>
+              </Paper>
+              <Paper className={classes.paperIcon}>
+                <Delete fontSize="large" className={classes.deleteY}/>
+              </Paper>
+            </div>
+          </Grid>
+          <Grid item xs={7} className={classes.gridItem}>
+            <Paper className={classes.paperInfos}>
+              <Typography variant="h5">Informations complémentaires</Typography>
+              <Typography variant="subtitle1">Les bouteilles en plastique transparentes ou opaques se recyclent toutes
+                et se
+                trient bien vidées avec leur bouchon. Vous pouvez les trier telles quelles ou aplaties dans leur
+                longueur et non à la verticale. Exemples : les bouteilles d'eau, de lait, de jus de fruit, de sodas, de
+                yaourt à boire, d'huile, de vinaigre mais également toutes les bouteilles en plastique de lessive
+                liquide, d'assouplissant, de nettoyant pour le sol et d'eau de javel (seule bouteille de produit
+                dangereux qui est accepté à condition d'être bien vidée).
+              </Typography>
+            </Paper>
+          </Grid>
+          <Grid item xs={2} className={classes.gridItemtype}>
+            <Typography color="primary" variant="h4">Bouchon</Typography>
+          </Grid>
+          <Grid item xs={3} className={classes.gridIcons}>
+            <Divider orientation="vertical"/>
+            <div className={classes.icons}>
+              <Paper className={classes.paperIcon}>
+                <Sync fontSize="large" className={classes.sync}/>
+              </Paper>
+              <Paper className={classes.paperIcon}>
+                <Delete fontSize="large" className={classes.deleteY}/>
+              </Paper>
+            </div>
+          </Grid>
+          <Grid item xs={7} className={classes.gridItem}>
+            <Paper className={classes.paperInfos}>
+              <Typography variant="h5">Informations complémentaires</Typography>
+              <Typography variant="subtitle1">Rennes soutient l'association "Un bouchon Un sourire" depuis 2000. Donnez
+                vos bouchons en plastique ou liège à un point de collecte, l'association les recyclera et reversera les
+                fonds à des projets pour les personnes handicapées. Vous trouverez un point de collecte à la biocoop 32
+                Boulevard de Groslay
+              </Typography>
+            </Paper>
+          </Grid>
+        </Grid>
+      )}
       {/*<ul><Typography variant="h6">Bouteille plastique: A TRIER - déposer dans une poubelle jaune</Typography></ul>*/}
       {/*<ul><Typography variant="h6">Bouteille plastique: A TRIER - déposer dans une poubelle jaune</Typography></ul>*/}
     </div>
