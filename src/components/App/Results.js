030e062b360f014eb6465f506fd8f3b62909bb6f
Quentin Michel <quentinmichel69110@gmail.com>
2020-02-13 14:04:24 +0100

    continue results

--- src/components/App/Results.js
+++ src/components/App/Results.js
@@ -1,6 +1,5 @@
 import React from "react";
 import Typography from "@material-ui/core/Typography";
-import {Webcam} from "./Webcam";
 import {makeStyles} from "@material-ui/styles";
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
@@ -10,6 +9,8 @@ import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 import SearchIcon from '@material-ui/icons/Search';
 import DirectionsIcon from '@material-ui/icons/Directions';
+import Button from "@material-ui/core/Button";
+import {Check, MyLocation, Place} from "@material-ui/icons";
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -34,52 +35,72 @@ const useStyles = makeStyles(theme => ({
   divider: {
     height: 28,
     margin: 4,
-  }
+  },
+  found: {
+    width: '100%',
+    display: 'flex',
+  },
+  foundText: {
+    marginLeft: 30,
+    marginRight: 50,
+  },
+  grid: {
+  },
+  gridItem: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  dividerContainer: {
+    display: 'block',
+    width: '100%',
+    marginTop: 40,
+    marginBottom: 40,
+  },
 }));
 
 export const Results = () => {
   const classes = useStyles();
   return (
     <div className={classes.root}>
-      <Typography variant="h2">Trouvé: Nettoyant multi surface</Typography>
-      
-      <Grid container spacing={3}>
-        <Grid item xs={4}>
-          <div className={classes.paper}>
-            <Typography variant="h5">Localisez moi</Typography>
-            <Paper component="form" className={classes.locationInput}>
-              <IconButton className={classes.iconButton} aria-label="menu">
-                <MenuIcon />
-              </IconButton>
-              <InputBase
-                className={classes.input}
-                placeholder="Search Google Maps"
-                inputProps={{ 'aria-label': 'search google maps' }}
-              />
-              <IconButton type="submit" className={classes.iconButton} aria-label="search">
-                <SearchIcon />
-              </IconButton>
-              <Divider className={classes.divider} orientation="vertical" />
-              <IconButton color="primary" className={classes.iconButton} aria-label="directions">
-                <DirectionsIcon />
-              </IconButton>
-            </Paper>
-          </div>
+      <div className={classes.found}>
+        <Typography variant="h2" color="primary" className={classes.foundText}><Check fontSize="large"
+                                                                                      color="primary"/> Trouvé
+          !</Typography>
+        <Typography variant="h2">Nettoyant multi surface</Typography>
+      </div>
+      <div className={classes.dividerContainer}>
+        <Divider orientation="horizontal"/>
+      </div>
+      <Grid container spacing={3} className={classes.grid}>
+        <Grid item xs={5} className={classes.gridItem}>
+          <Paper component="form" className={classes.locationInput}>
+            <IconButton className={classes.iconButton} aria-label="menu">
+              <Place/>
+            </IconButton>
+            <InputBase
+              className={classes.input}
+              placeholder="Chercher votre ville"
+            />
+            <IconButton type="submit" className={classes.iconButton} aria-label="search">
+              <SearchIcon/>
+            </IconButton>
+            <Divider className={classes.divider} orientation="vertical"/>
+            <IconButton color="primary" className={classes.iconButton} aria-label="directions">
+              <MyLocation/>
+            </IconButton>
+          </Paper>
         </Grid>
-        <Grid item xs={2}>
-          <div className={classes.paper}>
-            <Typography variant="h5">OU</Typography>
-          </div>
+        <Grid item xs={2} className={classes.gridItem}>
+          <Typography variant="h5">OU</Typography>
         </Grid>
-        <Grid item xs={6}>
-          <div className={classes.paper}>
-            <Typography variant="h5">Resultats généraux</Typography>
-              <ul><Typography variant="h6">Bouteille plastique: A TRIER - déposer dans une poubelle jaune</Typography></ul>
-              <ul><Typography variant="h6">Bouteille plastique: A TRIER - déposer dans une poubelle jaune</Typography></ul>
-              
-          </div>
+        <Grid item xs={5} className={classes.gridItem}>
+          <Button variant="contained" color="#eeeeee">Resultats généraux</Button>
         </Grid>
       </Grid>
+      <ul><Typography variant="h6">Bouteille plastique: A TRIER - déposer dans une poubelle jaune</Typography></ul>
+      <ul><Typography variant="h6">Bouteille plastique: A TRIER - déposer dans une poubelle jaune</Typography></ul>
     </div>
   );
 };
